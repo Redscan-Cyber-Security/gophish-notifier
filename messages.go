8d7c845df1dd33a2badf0512d35ff28953610245
jhemmingsrs <73246905+jhemmingsrs@users.noreply.github.com>
2021-10-28 17:41:34 +0100

    Added "Token" parameter for MFA support.
    Added MFA support.

--- messages.go
+++ messages.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"encoding/json"
 	"html/template"
 	"net/url"
 	"strings"
-
 	"github.com/ashwanthkumar/slack-go-webhook"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -20,6 +20,7 @@ var (
 
 type Sender interface {
 	SendSlack() error
+	SendTeams() error
 	SendEmail() error
 }
 
@@ -67,9 +68,12 @@ func NewEventDetails(detailsRaw []byte) (EventDetails, error) {
 	}
 	return details, nil
 }
-
 func (e EventDetails) ID() string {
-	return e.Payload.Get("id")
+	return e.Payload.Get("p")
+}
+
+func (e EventDetails) Token() string {
+        return e.Payload.Get("token")
 }
 
 func (e EventDetails) UserAgent() string {
@@ -87,6 +91,7 @@ type SubmittedDetails struct {
 	UserAgent  string
 	Username   string
 	Password   string
+	Token	   string
 }
 
 func NewSubmittedDetails(response WebhookResponse, detailsRaw []byte) (SubmittedDetails, error) {
@@ -102,6 +107,7 @@ func NewSubmittedDetails(response WebhookResponse, detailsRaw []byte) (Submitted
 		Email:      response.Email,
 		Username:   details.Payload.Get("username"),
 		Password:   details.Payload.Get("password"),
+		Token:	    details.Payload.Get("token"),
 	}
 	return submittedDetails, nil
 }
@@ -112,7 +118,7 @@ func (w SubmittedDetails) SendSlack() error {
 	attachment.AddField(slack.Field{Title: "ID", Value: w.ID})
 	attachment.AddField(slack.Field{Title: "Address", Value: slackFormatIP(w.Address)})
 	attachment.AddField(slack.Field{Title: "User Agent", Value: w.UserAgent})
-	if !viper.GetBool("slack.disable_credentials") {
+	if !viper.GetBool("disable_credentials") {
 		attachment.AddField(slack.Field{Title: "Email", Value: w.Email})
 		attachment.AddField(slack.Field{Title: "Username", Value: w.Username})
 		attachment.AddField(slack.Field{Title: "Password", Value: w.Password})
@@ -121,9 +127,18 @@ func (w SubmittedDetails) SendSlack() error {
 	return sendSlackAttachment(attachment)
 }
 
+func (w SubmittedDetails) SendTeams() error {
+	gophishBase := viper.GetString("base_url")
+	url := fmt.Sprintf("%s/campaigns/%d", gophishBase, w.CampaignID)
+	color := "#f05b4f"
+        msg := "<b>ID:</b> " + w.ID + "<br>" + "<b>IP:</b> " + w.Address + "<br>" + "<b>Email:</b> " + w.Email + "<br>" + "<b>Username:</b> " + w.Username + "<br>" + "<b>Password:</b> " + w.Password + "<br>" + "<b>MFA Token:</b> " + w.Token + "<br>" + "<b>User Agent:</b> " + w.UserAgent
+	message := msg + "<br><b>Campaign URL:</b> " + "<a href=%22" + url + "%22>" + url + "</a>"
+	return sendTeams("PhishBot - Credentials Submitted", message, color)
+}
+
 func (w SubmittedDetails) SendEmail() error {
 	templateString := viper.GetString("email_submitted_credentials_template")
-	body, err := getEmailBody(templateString, w)
+        body, err := getEmailBody(templateString, w)
 	if err != nil {
 		return err
 	}
@@ -164,6 +179,12 @@ func (w ClickDetails) SendSlack() error {
 	}
 	attachment = addCampaignButton(attachment, w.CampaignID)
 	return sendSlackAttachment(attachment)
+}
+
+func (w ClickDetails) SendTeams() error {
+	color := "#ffa500"
+	message := "<b>ID:</b> " + w.ID + "<br>" + "<b>IP:</b> " + w.Address + "<br>" + "<b>Email:</b> " + w.Email + "<br>" + "<b>User Agent:</b> " + w.UserAgent
+        return sendTeams("PhishBot - Email Clicked", message, color)
 }
 
 func (w ClickDetails) SendEmail() error {
@@ -222,6 +243,13 @@ func (w OpenedDetails) SendSlack() error {
 	attachment = addCampaignButton(attachment, w.CampaignID)
 	return sendSlackAttachment(attachment)
 }
+
+func (w OpenedDetails) SendTeams() error {
+	color := "#ffff00"
+	message := "<b>ID:</b> " + w.ID + "<br>" + "<b>IP:</b> " + w.Address + "<br>" + "<b>Email:</b> " + w.Email + "<br>" + "<b>User Agent:</b> " + w.UserAgent
+	return sendTeams("PhishBot - Email Opened", message, color)
+}
+
 
 func (w OpenedDetails) SendEmail() error {
 	templateString := viper.GetString("email_send_click_template")
